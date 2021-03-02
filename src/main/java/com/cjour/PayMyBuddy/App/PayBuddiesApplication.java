c2640b0feb5f936d0e1f98d3949132935d431e93
Clément <jour.clement@gmail.com>
2021-03-02 10:55:28 +0100

    Project creation

---
+++ src/main/java/com/cjour/PayMyBuddy/App/PayBuddiesApplication.java
@@ -0,0 +1,13 @@
+package com.cjour.PayMyBuddy.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayBuddiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayBuddiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cjour/PayMyBuddy/App/PayBuddiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjour.PayMyBuddy.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayBuddiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
